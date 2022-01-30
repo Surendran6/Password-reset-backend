0a04f3d9843dd7a3b3a19366827026e084afa27c
surendran <surendransakthivel6@gmail.com>
2022-01-30 14:48:14 +0530

    update

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -2,21 +2,36 @@ const nodemailer = require('nodemailer');
 
 
 const sendEmail = (options) => {
-   
-    var transporter = nodemailer.createTransport({
-        host: "smtp-mail.outlook.com", // hostname
-        secureConnection: false, // TLS requires secureConnection to be false
-        port: 587, // port for secure SMTP
-        tls: {
-        ciphers:'SSLv3'
-        },
-        
-        requireTLS:true,
+
+    const transporter = nodemailer.createTransport({
+        service : "Hotmail",
         auth : {
             user : "dran.customer96@hotmail.com",
-            pass : process.env.EMAIL_PASSWORD 
+            pass : process.env.EMAIL_PASSWORD,
         }
     })
+    const mailDetails = {
+        from : "dran.customer96@hotmail.com",
+        to : options.to,
+        subject : options.subject,
+        html : options.text
+    }
+    
+   
+    // var transporter = nodemailer.createTransport({
+    //     host: "smtp-mail.outlook.com", // hostname
+    //     secureConnection: false, // TLS requires secureConnection to be false
+    //     port: 587, // port for secure SMTP
+    //     tls: {
+    //     ciphers:'SSLv3'
+    //     },
+        
+    //     requireTLS:true,
+    //     auth : {
+    //         user : "dran.customer96@hotmail.com",
+    //         pass : process.env.EMAIL_PASSWORD 
+    //     }
+    // })
 
     transporter.sendMail(mailDetails,function(err,data){
         if(err){
